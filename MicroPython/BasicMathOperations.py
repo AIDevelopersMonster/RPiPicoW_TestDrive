622a01c5740992e558295cfa1e355700a250f687
AlexMalachevsky <t89257409519@gmail.com>
2025-03-06 16:49:16 +0300

    Create BasicMathOperations.py

---
+++ MicroPython/BasicMathOperations.py
@@ -0,0 +1,55 @@
+# Post http://kontakts.ru/showthread.php/40902?p=86263#post86263
+# Video https://youtu.be/13iYW1CO7-0
+# Telega https://t.me/MrMicroPython
+# =============================================================
+# Ввод чисел с клавиатуры
+a = int(input("Введите первое число (a): "))
+b = int(input("Введите второе число (b): "))
+
+# Сложение
+sum_ab = a + b
+
+# Вычитание
+diff_ab = a - b
+
+# Умножение
+mult_ab = a * b
+
+# Деление
+div_ab = a / b
+
+# Целочисленное деление
+floor_div_ab = a // b
+
+# Остаток от деления (модуль)
+mod_ab = a % b
+
+# Возведение в степень
+pow_ab = a ** b
+
+# Абсолютное значение
+abs_ab = abs(a)
+
+# Округление числа
+rounded_ab = round(a)
+
+# Целая часть (округление вниз)
+floor_value = int(a)
+
+# Квадратный корень
+sqrt_ab = a ** 0.5
+
+# Вывод результатов
+print("\nРезультаты:")
+print("Сложение:", sum_ab)
+print("Вычитание:", diff_ab)
+print("Умножение:", mult_ab)
+print("Деление:", div_ab)
+print("Целочисленное деление:", floor_div_ab)
+print("Остаток от деления:", mod_ab)
+print("Возведение в степень:", pow_ab)
+print("Абсолютное значение:", abs_ab)
+print("Округление:", rounded_ab)
+print("Целая часть:", floor_value)
+print("Квадратный корень:", sqrt_ab)
+
