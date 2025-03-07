170a41c5e920a599d4d7069fce98ca7fb26d4d0e
AlexMalachevsky <t89257409519@gmail.com>
2025-03-07 18:11:08 +0300

    Create WebedsBtns_Control.py

---
+++ MicroPython/WebedsBtns_Control.py
@@ -0,0 +1,64 @@
+import wifi_init
+import socket
+from machine import Pin
+
+# Инициализация встроенного светодиода
+led = Pin("LED", Pin.OUT)
+
+# Подключаемся к Wi-Fi
+wlan = wifi_init.connect_wifi()
+
+# Настройка веб-сервера
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(addr)
+s.listen()
+
+print("Сервер запущен, слушает по адресу", addr)
+
+# Основной цикл обработки HTTP-запросов
+while True:
+    try:
+        conn, addr = s.accept()
+        print("Новое подключение от", addr)
+
+        # Читаем запрос
+        request = conn.recv(1024).decode()
+        print("Запрос:", request)
+
+        # Обрабатываем команды
+        if "/lighton" in request:
+            led.value(1)
+        elif "/lightoff" in request:
+            led.value(0)
+
+        # Формируем HTML-страницу
+        response = """\
+HTTP/1.0 200 OK
+Content-Type: text/html
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pico Web Server</title>
+</head>
+<body>
+    <h1>LED Control</h1>
+    <form action="/lighton">
+        <button>Turn On</button>
+    </form>
+    <br>
+    <form action="/lightoff">
+        <button>Turn Off</button>
+    </form>
+</body>
+</html>
+"""
+        conn.sendall(response.encode())
+        conn.close()
+
+    except OSError:
+        conn.close()
+        print("Соединение закрыто")
+
